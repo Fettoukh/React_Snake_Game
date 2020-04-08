8db1a88fefa0bfbb3bf37d4b0d659a8627b16875
Fettoukh <medamine.fettoukh@gmail.com>
2020-04-09 00:53:52 +0200

    Updates

---
+++ precache-manifest.9479992a91dc21eb2f3d52b4f1c2839a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8a152f1c8966015a0993d307cc29d6",
+    "url": "/React_Snake_Game/index.html"
+  },
+  {
+    "revision": "ea68fcb3ec2236783c0c",
+    "url": "/React_Snake_Game/static/css/main.88669693.chunk.css"
+  },
+  {
+    "revision": "b48da30710d20ff1a147",
+    "url": "/React_Snake_Game/static/js/2.85a59131.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Snake_Game/static/js/2.85a59131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea68fcb3ec2236783c0c",
+    "url": "/React_Snake_Game/static/js/main.013d9dc6.chunk.js"
+  },
+  {
+    "revision": "c2a08e9d86efc8753cc0",
+    "url": "/React_Snake_Game/static/js/runtime-main.0bb51ff9.js"
+  }
+]);
